c2414bd0bfa80e7cd285d4aa599e519c5b3f6b2f
Jovnnaalex <jaguttorm@uia.no>
2025-10-22 13:05:35 +0200

    lalala

--- Main.py
+++ Main.py
@@ -1,1 +1,15 @@
-# Gruppe-innlevering-2-25-november
+# FORSLAG TIL STRUKTUR
+#1. Introduksjon: Kort bakgrunn om situasjonen i prosjektet og hvilke konflikter som
+#ulmer.
+
+#2. Beslutningspunkt 1: Hvordan håndterer Erling konflikten mellom Silje og Sivert?
+#a. Alternativ A → konsekvens
+#b. Alternativ B → konsekvens
+
+#3. Beslutningspunkt 2: Hvordan håndterer han uenigheten mellom Hamdi og Jabir?
+#a. Alternativ A → konsekvens
+#b. Alternativ B → konsekvens
+
+#4. Beslutningspunkt 3: Hvordan bevarer han motivasjonen i teamet?
+#a. Alternativ A → konsekvens
+#b. Alternativ B → konsekvens
