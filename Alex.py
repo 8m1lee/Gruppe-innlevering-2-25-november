b7c03ebd06c65c6af9221535a87cf375144c08bc
Jovnnaalex <jaguttorm@uia.no>
2025-10-24 13:26:33 +0200

    Ali

---
+++ Alex.py
@@ -0,0 +1,32 @@
+
+#1. Introduksjon: Kort bakgrunn om situasjonen i prosjektet og hvilke konflikter som
+#ulmer.
+
+#2. Beslutningspunkt 1: Hvordan håndterer Erling konflikten mellom Silje og Sivert?
+#a. Alternativ A → konsekvens
+#b. Alternativ B → konsekvens
+
+#3. Beslutningspunkt 2: Hvordan håndterer han uenigheten mellom Hamdi og Jabir?
+#a. Alternativ A → konsekvens
+#b. Alternativ B → konsekvens
+
+#4. Beslutningspunkt 3: Hvordan bevarer han motivasjonen i teamet?
+#a. Alternativ A → konsekvens
+#b. Alternativ B → konsekvens
+
+#Variabler
+key = True
+
+#Introduksjon
+print("""Velkommen til beslutningsspillet om prosjektledelse!
+Her skal du ta på deg rollen som prosjektleder Erling, som står overfor flere utfordringer i teamet sitt.  
+Dine valg vil påvirke utfallet av prosjektet og teamets dynamikk. Lykke til!""")
+
+while True: 
+    if key == True:
+        print ("fart")
+    elif key == False:
+        print("pause")
+
+    else:
+        print("stopp")
